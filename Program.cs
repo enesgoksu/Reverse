ba941440aeba1068018c5fd39fbd6f7d8da55f14
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-04-05 15:43:55 +0300

    program.cs değiştirildi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Reverse.Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cümle veya kelime giriniz: ");
+            string ifade = Console.ReadLine();
+            string bosluk = "";
+
+            for (int i = ifade.Length - 1; i >= 0; i--)
+            {
+                bosluk += ifade[i];
+
+            }
+            Console.WriteLine(bosluk);
+        }
+    }
+}
